22873e76901397b14910a156e7c589bb134665c6
Andy Long <andy@theouteredge.co.uk>
2013-02-23 00:34:12 +0000

    Renaming
    Renamed the FleIndexer class to KeyFileIndex which is a bit more
    desciptive of its purpose
    
    Renamed the end variables to length, so they are describing what they
    are. as they dont represent the end position but the amount of bytes to
    read

--- src/PersistantCache.FileHandlingTests/Program.cs
+++ src/PersistantCache.FileHandlingTests/Program.cs
@@ -11,16 +11,14 @@ namespace PersistantCache.FileHandlingTests
 {
     class Program
     {
-        private static FileIndex _index;
+        private static KeyFileIndex _keyIndex;
 
         static void Main(string[] args)
         {
-
-
             var key = Hash(Guid.NewGuid().ToString());
             var length = key.ToUtf8Bytes().Length;
 
-            _index = new FileIndex("C:\\tmp\\PersistentCache", length);
+            _keyIndex = new KeyFileIndex("C:\\tmp\\PersistentCache", length);
 
             for (var i = 0; i < 200; i++)
             {
@@ -33,20 +31,20 @@ namespace PersistantCache.FileHandlingTests
         private static void WriteAKeyToTheIndex(int i)
         {
             var result = false;
-            int end;
+            int length;
             int start;
 
             var key = Hash(Guid.NewGuid().ToString());
 
-            _index.Add(key, 1000 * i, 10000 * i);
+            _keyIndex.Add(key, 1000 * i, 10000 * i);
 
-            Console.WriteLine(_index.Contains(key)
+            Console.WriteLine(_keyIndex.Contains(key)
                                   ? "We found the {0} key in the file :D"
                                   : "We couldn't find the {0} key in the file O_o", i + 1);
 
 
-            result = _index.GetDataPosition(key, out start, out end);
-            if (start == 1000 * i && end == 10000 * i)
+            result = _keyIndex.GetDataPosition(key, out start, out length);
+            if (start == 1000 * i && length == 10000 * i)
                 Console.WriteLine("Writing {0} Entry Works!", i + 1);
             else
                 Console.WriteLine("Writing {0} Entry Failed! O_o", i + 1);
