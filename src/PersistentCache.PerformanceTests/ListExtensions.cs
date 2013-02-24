2fb5a4c05e18d6ea7f873cdab59916e7f6e47e2d
Andy Long <andy@theouteredge.co.uk>
2013-02-24 17:34:01 +0000

    Switched Direction
    So the DiskCache didn't work out, it was very very very slow! So its
    been deleted.
    
    But I did abstract the FileCaching into an Interface ICacheToDisk and
    tried another implementation which uses directories. The directory cache
    is much faster than the File one. This is now the default implemetation.
    
    Found a bug while actracting the DiskCache where the previous code was
    checing for files in c:\ and not c:\baseDirectory, so it was constantly
    saving items to the disk.

---
+++ src/PersistentCache.PerformanceTests/ListExtensions.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PersistentCache.PerformanceTests
+{
+    public static class ListExtensions
+    {
+        static readonly Random Random = new Random(DateTime.Now.Millisecond);
+
+        public static void Shuffle<T>(this IList<T> list)
+        {
+            var n = list.Count;
+            while (n > 1)
+            {
+                n--;
+
+                var k = Random.Next(n + 1);
+                var value = list[k];
+
+                list[k] = list[n];
+                list[n] = value;
+            }
+        }
+    }
+}
--- src/PersistentCache/CacheStore.cs
+++ src/PersistentCache/CacheStore.cs
@@ -4,32 +4,62 @@ using System.IO;
 using System.Runtime.Caching;
 using System.Security.Cryptography;
 using System.Text;
+using PersistentCache.DiskCache;
 using ServiceStack.Text;
 
 namespace PersistentCache
 {
-    public class CacheStore
+    /// <summary>
+    /// AUTHOR
+    /// ------
+    /// Andy Long | @theouteredge | andy@theouteredge.co.uk
+    /// 
+    /// ABOUT
+    /// -----
+    /// A Persistent Cache. Once a key/value is stored in the cache its stored for the lifetime of the cache.
+    /// This is designed to be used in a very specific use case where you would want to large cache persistant cache which would be larger than the available
+    /// system memory and you want the cache to store all the values while performing a single long running process. Once the process has completed the
+    /// cache should be disposed of.
+    /// 
+    /// Once the available memory starts to run out and items are evicted or expired from the in memory cache they will be persisted to the disk.
+    /// 
+    /// DEPENDANCIES
+    /// ------------
+    /// ServiceStack.Text
+    /// </summary>
+    public class CacheStore : IDisposable
     {
         private readonly MemoryCache _cache;
+        private readonly ICacheToDisk _diskCache;
+        private bool _itemsCachedToDisk = false;
 
-        public string PollingInterval { get; set; }
-        public string PhysicalMemoryLimitPercentage { get; set; }
-        public string CacheMemoryLimitMegabytes { get; set; }
+
+        public string PollingInterval { get; private set; }
+        public string PhysicalMemoryLimitPercentage { get; private set; }
+        public string CacheMemoryLimitMegabytes { get; private set; }
 
         public string BaseDirectory { get; set; }
 
 
 
-        public CacheStore()
+        public CacheStore(string baseDirectory, string cacheMemoryLimitMegabytes = null, string physicalMemoryLimitPercentage = null, string pollingInterval = null)
         {
+            BaseDirectory = baseDirectory;
+
             var config = new NameValueCollection
                 {
-                    {"pollingInterval", PollingInterval ?? "00:01:00" },
-                    {"physicalMemoryLimitPercentage", PhysicalMemoryLimitPercentage ?? "0" },
-                    {"cacheMemoryLimitMegabytes", CacheMemoryLimitMegabytes ?? "100" }
+                    {"pollingInterval", pollingInterval ?? "00:01:00" },
+                    {"physicalMemoryLimitPercentage", physicalMemoryLimitPercentage ?? "0" },
+                    {"cacheMemoryLimitMegabytes", cacheMemoryLimitMegabytes ?? "100" }
                 };
 
             _cache = new MemoryCache("MainCache", config);
+            
+            //_diskCache = new FileCache(BaseDirectory);
+            _diskCache = new DirectoryCache(BaseDirectory);
+
+            //var keyLength = Hash(Guid.NewGuid().ToString()).ToUtf8Bytes().Length;
+            //_diskCache = new DiskCache(BaseDirectory, keyLength);
         }
 
 
@@ -50,12 +80,11 @@ namespace PersistentCache
                 return true;
             }
 
-            // it wasn't in the memory cache, so look for a file with the keys name
-            var filename = GetSafeFileName(key);
-            if (File.Exists(filename))
+            if (_itemsCachedToDisk)
             {
-                value = File.ReadAllText(BaseDirectory + filename).FromJson<T>();
-                return true;
+                // it wasn't in the memory cache, so look for it within the ICacheToDisk
+                if (_diskCache.TryGet<T>(key, out value))
+                    return true;
             }
 
             value = default(T);
@@ -68,16 +97,12 @@ namespace PersistentCache
             // if we are not removing the item cos it was removed by the caller then save it to disk
             if (arguments.RemovedReason == CacheEntryRemovedReason.Evicted || arguments.RemovedReason == CacheEntryRemovedReason.Expired)
             {
-                File.WriteAllText(Path.Combine(BaseDirectory, GetSafeFileName(arguments.CacheItem.Key)), arguments.CacheItem.Value.ToJson());
+                _itemsCachedToDisk = true;
+                _diskCache.Put(arguments.CacheItem.Key, arguments.CacheItem.Value);
             }
         }
 
-        private static string GetSafeFileName(string filename)
-        {
-            Array.ForEach(Path.GetInvalidFileNameChars(), c => filename = filename.Replace(c.ToString(), "_"));
-            return filename + ".cache";
-        }
-
+        
         private static string Hash(string s)
         {
             var hash = new SHA1Managed();
@@ -89,6 +114,15 @@ namespace PersistentCache
             result = Convert.ToBase64String(hashedBuffer);
 
             return result;
+        }
+
+        public void Dispose()
+        {
+            if (_diskCache != null)
+                _diskCache.Dispose();
+
+            if (_cache != null)
+                _cache.Dispose();
         }
     }
 }
